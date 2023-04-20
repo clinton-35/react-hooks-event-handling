9d69916f421d3fdf900842d7c474dfc4f1af1bd1
clinton <clinton.wambua@student.moringaschool.com>
2023-04-20 07:44:01 +0300

    complete

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
-  }
 
-  return <button>Tickle me!</button>;
+function Tickler() {
+    function tickle(event){
+    console.log(event);
+
+    }
+
+  return <button onClick={tickle} >Tickle me!</button>;
 }
 
 export default Tickler;
